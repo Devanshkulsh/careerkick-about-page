7a474a9967c323a079eca810a53fd80c664ebf75
Devansh <devkulsh1308@gmail.com>
2026-04-17 12:16:47 +0530

    vercel deployment check

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,21 +1,21 @@
 /**
  * Root About page composition for CareerKick.
  */
-import { MotionConfig } from 'framer-motion'
-import { memo } from 'react'
-import BrandStory from '../components/about/BrandStory'
-import CTABanner from '../components/about/CTABanner'
-import HeroSection from '../components/about/HeroSection'
+import { MotionConfig } from "framer-motion";
+import { memo } from "react";
+import BrandStory from "../components/about/BrandStory";
+import CTABanner from "../components/about/CTABanner";
+import HeroSection from "../components/about/HeroSection";
 // import ImpactStats from '../components/about/ImpactStats'
-import JourneyTimeline from '../components/about/JourneyTimeline'
-import LeadershipTeam from '../components/about/LeadershipTeam'
-import MissionVision from '../components/about/MissionVision'
-import OfficeLocations from '../components/about/OfficeLocations'
+import JourneyTimeline from "../components/about/JourneyTimeline";
+import LeadershipTeam from "../components/about/LeadershipTeam";
+import MissionVision from "../components/about/MissionVision";
+import OfficeLocations from "../components/about/OfficeLocations";
 // import ServicesGrid from '../components/about/ServicesGrid'
-import TestimonialMarquee from '../components/about/TestimonialMarquee'
-import WhyChooseUs from '../components/about/WhyChooseUs'
-import YouTubeChannels from '../components/about/YouTubeChannels'
-import FeatureSection from '../components/about/FeatureSection'
+import TestimonialMarquee from "../components/about/TestimonialMarquee";
+import WhyChooseUs from "../components/about/WhyChooseUs";
+import YouTubeChannels from "../components/about/YouTubeChannels";
+import FeatureSection from "../components/about/FeatureSection";
 
 function AboutPageComponent() {
   return (
@@ -38,8 +38,7 @@ function AboutPageComponent() {
         {/* <ServicesGrid /> */}
       </main>
     </MotionConfig>
-  )
+  );
 }
 
-export default memo(AboutPageComponent)
-
+export default memo(AboutPageComponent);
